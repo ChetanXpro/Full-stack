b38316237036f1b365e2274908f53fa5dea74ba7
chetan <chetan@chetans-MacBook-Air.local>
2023-10-24 21:20:28 +0530

    feat: debug

--- client/src/Api/api.ts
+++ client/src/Api/api.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log("API BASE URL", API_BASE_URL);
+
 export const apiInstance = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
